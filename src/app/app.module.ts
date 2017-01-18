0f297f66f78e9201da53b609e1730ccfd12757ce
nikohol <nikohol@metropolia.fi>
2017-01-18 13:40:33 +0200

    new ng components

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -4,10 +4,18 @@ import { FormsModule } from '@angular/forms';
 import { HttpModule } from '@angular/http';
 
 import { AppComponent } from './app.component';
+import { NoteCardComponent } from './note-card/note-card.component';
+import { MainComponent } from './main/main.component';
+import { NotesComponent } from './notes/notes.component';
+import { TopBarComponent } from './top-bar/top-bar.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    NoteCardComponent,
+    MainComponent,
+    NotesComponent,
+    TopBarComponent
   ],
   imports: [
     BrowserModule,
